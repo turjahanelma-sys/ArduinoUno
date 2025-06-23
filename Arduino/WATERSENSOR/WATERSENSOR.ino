6387a001ee4aa69f9b523a65d34738d6301565fe
turjahanelma-sys <2021-2-60-065@std.ewubd.edu>
2025-06-23 12:13:13 +0600

    Add files via upload

---
+++ Arduino/WATERSENSOR/WATERSENSOR.ino
@@ -0,0 +1,7 @@
+void setup(){
+Serial.begin(9600);     // Communication started with  9600 baud
+}
+void loop(){
+int sensor=analogRead(A0); // Incoming analog  signal read and appointed sensor
+Serial.println(sensor);   //Wrote serial port
+}
